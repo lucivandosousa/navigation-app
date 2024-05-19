21f5e31b0c78efcf885b21da99f765b00479c3de
Lucivando Sousa <lucivandosousa@gmail.com>
2024-05-19 19:49:57 -0300

    chore: clean dead code

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,6 +1,5 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import TabRoutes from './tab.routes'
 import Profile from '../screens/Profile'
 
 const Stack = createNativeStackNavigator()
