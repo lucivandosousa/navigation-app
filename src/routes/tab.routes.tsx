511000b3046c95ebaeb00dffc8e29e24462954d2
Lucivando Sousa <lucivandosousa@gmail.com>
2024-05-19 18:42:24 -0300

    feat: create tab navigation

---
+++ src/routes/tab.routes.tsx
@@ -0,0 +1,40 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { Feather } from "@expo/vector-icons"
+
+import Home from "../screens/Home"
+import New from "../screens/New"
+
+const Tab = createBottomTabNavigator()
+
+export default function TabRoutes() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: "#9999FF"
+        },
+        tabBarActiveTintColor: "#ffffff",
+        tabBarInactiveTintColor: "#CCCCFF"
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, size}) => <Feather name="home" color={color} size={size}/>,
+          tabBarLabel: "Início"
+        }}
+      />
+      <Tab.Screen
+        name="New"
+        component={New}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, size}) => <Feather name="plus" color={color} size={size}/>,
+          tabBarLabel: "Novo"
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
---
+++ src/screens/Home.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Feed() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home</Text> 
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold'
+  }
+})
